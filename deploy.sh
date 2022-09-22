96e85db006f73ba30e5e383cf40373deea9763f0
sgtcoder <usmccoder@gmail.com>
2022-09-22 14:09:16 -0700

    deploy updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="dnsmasq"
-docker build --pull --no-cache --tag $REGISTRY/$PROJECT_NAME .
+
+docker build  -t $REGISTRY/$PROJECT_NAME .
 docker push $REGISTRY/$PROJECT_NAME
 
