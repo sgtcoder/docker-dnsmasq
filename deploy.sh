093cbfd1664a5ddf3c8805839f60d40e88b0b974
coder <usmccoder@gmail.com>
2023-05-29 20:20:58 -0700

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="dnsmasq"
 
-docker build  -t $REGISTRY/$PROJECT_NAME .
+docker build --no-cache -t $REGISTRY/$PROJECT_NAME . || exit 1
 docker push $REGISTRY/$PROJECT_NAME
-
